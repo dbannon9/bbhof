22ced0e986e7ea57dc787d4db0edfaf4ee4402f5
Dan Bannon <danbannon9@gmail.com>
2026-05-16 14:52:25 -0400

    player pages and testing

--- app/bbhof-app.py
+++ app/bbhof-app.py
@@ -10,11 +10,9 @@ st.set_page_config(layout="wide",page_title="Baseball Hall of Fame Tracker")
 
 
 def fetch_table_data(table_name):
-    response = neon.client.table(table_name).select("*").execute()
-
+    data = neon.query(f"SELECT * FROM {table_name}", ttl=0)
     # Supabase v2 client: actual rows are in response.data
-    data = response.data
-    if not data:
+    if data.empty:
         st.warning(f"No data returned from table '{table_name}'.")
         return pd.DataFrame()
 
@@ -25,4 +23,37 @@ def fetch_table_data(table_name):
 players = fetch_table_data(table_name='players')
 
 st.title("Baseball Hall of Fame Tracker")
-st.dataframe()
+
+players["fangraphs_link"] = ("http://www.fangraphs.com/statss.aspx?playerid=" + players["fangraphs_id"].astype(str).str.lstrip('0'))
+players["bref_link"] = ("https://www.baseball-reference.com/players/" + players['player_last'].str[:1].str.lower() + "/" + players["bref_id"].astype(str)) + ".shtml"
+players["mlb_link"] = ("https://www.mlb.com/player/" + players["mlb_id"].astype(str))
+
+players_display = players[['player_name', 'first_year_on_ballot', 'fangraphs_link','bref_link','mlb_link']].rename(columns={
+    'player_name': 'Player',
+    'first_year_on_ballot': 'First Eligible Year',
+    'fangraphs_link': 'Fangraphs',
+    'bref_link': 'Baseball Reference',
+    'mlb_link': 'MLB'
+})
+
+st.dataframe(
+    players_display,
+    hide_index=True,
+    column_config={
+        "Fangraphs": st.column_config.LinkColumn(
+            "Fangraphs",
+            help="Click this link to view the player's Fangraphs page",
+            display_text="Fangraphs"
+        ),
+        "Baseball Reference": st.column_config.LinkColumn(
+            "Baseball Reference",
+            help="Click this link to view the player's Baseball Reference page",
+            display_text="BRef"
+        ),
+        "MLB": st.column_config.LinkColumn(
+            "MLB",
+            help="Click this link to view the player's MLB page",
+            display_text="MLB"
+        )
+    }
+)
